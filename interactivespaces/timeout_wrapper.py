1deb4c3646d75b4282e7d9d99600e9590c4ddde9
Joshua Tolley <josh@endpoint.com>
2014-12-08 09:07:08 -0700

    Should have added this in the last commit

---
+++ interactivespaces/timeout_wrapper.py
@@ -0,0 +1,14 @@
+from contextlib import contextmanager
+import signal
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException, "Timed out!"
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
